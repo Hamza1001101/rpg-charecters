39519012519c4695dd80f69ee056ab6aabb478f8
Hamza Farah <hamza.farah@se.experis.com>
2021-07-15 22:15:21 +0200

    added some unit testing (first-patch)

---
+++ test/charecters/CharecterTests.java
@@ -0,0 +1,38 @@
+package charecters;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharecterTests {
+
+    private Mage mage;
+    private Rogue rogue;
+    private Warrior warrior;
+    private Ranger ranger;
+
+    @BeforeEach
+    public void initializeCharacters(){
+        mage = new Mage("Aegwynn");
+    }
+
+    @Test
+    public void getLevel_InitializedCharecter_StartingLevelOne(){
+        assertEquals(1, mage.getLevel());
+    }
+
+    @Test
+    public void levelUp_IncreaseLevelByOne_ShouldIncrementByOne(){
+        mage.levelUp(1);
+        assertEquals(2, mage.getLevel());
+    }
+    @Test
+    public void levelUp_ByZeroOrLess_ShouldYieldException(){
+        assertThrows(IllegalArgumentException.class, () -> {
+            mage.levelUp(0);
+            mage.levelUp(-1);
+        });
+    }
+
+}
